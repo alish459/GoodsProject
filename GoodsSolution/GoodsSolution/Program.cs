a464a79b330e2fbd4744f3a57a99ed520e2f1a1d
Ali Sharafzade <ali.sharafzade1991@gmail.com>
2019-03-17 10:38:50 +0430

    Sync

--- GoodsSolution/GoodsSolution/Program.cs
+++ GoodsSolution/GoodsSolution/Program.cs
@@ -16,7 +16,7 @@ namespace GoodsSolution
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
